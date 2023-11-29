13acb60312e04d355078ce9b65bc7b70e8b5090d
Hippolyte Claude <hclaude@k2r3p1.42mulhouse.fr>
2023-11-29 12:54:56 +0100

    FAUT DEBUG

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,12 @@
 /*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:35:06 by hclaude           #+#    #+#             */
-/*   Updated: 2023/11/29 11:56:25 by hclaude          ###   ########.fr       */
+/*   Updated: 2023/11/29 12:49:50 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-
-char *get_next_line(int fd)
-{
-	char buffer[BUFFER_SIZE];
-	static char *old_buffer;
-	static int	i = 0;
-	
-	if (fd == -1 || read(fd, buffer, BUFFER_SIZE) <= 0)
-	{
-		if (old_buffer == NULL)
-			return (NULL);
-		return (old_buffer);
-	}
-	if (buffer != NULL && old_buffer != NULL)
-		get_line(buffer, old_buffer);
-	else
-		old_buffer = get_futurline(buffer);
-		return (ft_substr(buffer, 0, get_backslash(buffer)));
-}
 
 static int get_backslash(char *str)
 {
@@ -48,5 +29,59 @@ static char *get_line(char *newbuffer, char *oldbuffer)
 	int		len;
 
 	len = ft_strlen(oldbuffer) + get_backslash(newbuffer);
-	dst = 
+	dst = ft_calloc(len + 1, sizeof(char));
+	if (!dst)
+		return (NULL);
+	ft_strlcat(dst, oldbuffer, ft_strlen(oldbuffer));
+	ft_strlcat(dst, newbuffer, get_backslash(newbuffer));
+	free(oldbuffer);
+	return (dst);
+}
+
+static char *get_futurline(char *buffer)
+{
+	int	start;
+	int i;
+	char *newbuffer;
+	
+	start = get_backslash(buffer) + 1;
+	i = start;
+	while (buffer[i] != '\0' || buffer[i] != '\n')
+		i++;
+	newbuffer = ft_calloc(i + 1, sizeof(char));
+	if (!newbuffer)
+		return (NULL);
+	i = 0;
+	while (buffer[start] != '\0' || buffer[start] != '\n')
+	{
+		newbuffer[i] = buffer[start];
+		i++;
+		start++;
+	}
+	return(newbuffer);
+}
+
+char *get_next_line(int fd)
+{
+	char buffer[BUFFER_SIZE];
+	static char *old_buffer;
+	char *tempbuffer;
+
+	if (fd == -1 || read(fd, buffer, BUFFER_SIZE) <= 0)
+	{
+		if (old_buffer == NULL)
+			return (NULL);
+		return (old_buffer);
+	}
+	if (buffer && old_buffer)
+	{
+		tempbuffer = old_buffer;
+		
+		get_line(buffer, old_buffer);
+	}
+	else
+	{
+		old_buffer = get_futurline(buffer);
+		return (ft_substr(buffer, 0, get_backslash(buffer)));
+	}
 }
